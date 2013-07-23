cd877c2435f4b327b3018d73e3c64199ce70cf69
a0-prw <pete_wood@runbox.com>
2013-07-23 10:35:04 +0200

    README added.

--- cl-paymill.asd
+++ cl-paymill.asd
@@ -30,7 +30,7 @@
 (defsystem "cl-paymill"
 
     :description "CL-PAYMILL is a common lisp interface to the Paymill
-    payment service API.  See https://www.paymill.com"
+    payment service API.  See https://www.paymill.com/"
 
     :version "0.0.1"
     :author "Peter Wood, email: pete_wood at runbox.com"
--- paymill.lisp
+++ paymill.lisp
@@ -30,17 +30,16 @@
 ;;*PAYMILL-KEY* needs to be set to your private test key for testing.  If you
 ;;set it to your live key - YOU WILL BE CHARGED FOR THE TRANSACTIONS.
 
-;;Until you call INITIALIZE-PAYMILL, the interface is not defined.
-
 (defun initialize-paymill (key)
   "Call this function with your private test- or live-key to define
 access functions to the Paymill API.  If you use a live-key, you will
 be charged for any transactions which are made."
-  (setf *paymill-key* key)
-  (define-resource-access :clients (:new :update :retrieve :list :delete))
-  (define-resource-access :payments (:new :retrieve :list :delete))
-  (define-resource-access :transactions (:new :update :retrieve :list))
-  (define-resource-access :refunds (:refund :retrieve :list))
-  (define-resource-access :offers (:new :update :retrieve :list :delete))
-  (define-resource-access :subscriptions (:new :retrieve :update :list :delete)))
+  (setf *paymill-key* key))
+
+(define-resource-access :clients (:new :update :retrieve :list :delete))
+(define-resource-access :payments (:new :retrieve :list :delete))
+(define-resource-access :transactions (:new :update :retrieve :list))
+(define-resource-access :refunds (:refund :retrieve :list))
+(define-resource-access :offers (:new :update :retrieve :list :delete))
+(define-resource-access :subscriptions (:new :retrieve :update :list :delete))
 
